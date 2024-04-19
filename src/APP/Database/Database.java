6938d2a5595683c0e38f5ee23174f8975963642f
TroubledDreamer <carlyonja@gmail.com>
2024-04-18 23:45:50 -0500

    andre nedd to this the data base as the jar file is not implemeted

---
+++ src/APP/Database/Database.java
@@ -0,0 +1,54 @@
+package APP.Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Database {
+
+    public static void main(String[] args) {
+
+        String url = "jdbc:mysql://http://sql5.freesqldatabase.com:3306/sql5700240";
+        String username = "sql5700240";
+        String password = "1rFJ2HigUE";
+
+
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection con = DriverManager.getConnection(url, username, password);
+
+            Statement statement = con.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM OrderTable");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3) + " " + resultSet.getString(4)  + " " + resultSet.getInt(5) + " " + resultSet.getString(6) + " " + resultSet.getString(7)  + " " + resultSet.getInt(8));
+            }
+
+            connection.close();
+
+            
+        } catch (SQLException e) {
+            System.out.println("Database connection failed");
+            e.printStackTrace();
+
+        }
+
+
+        
+
+
+
+
+
+
+
+    }
+
+
+    
+}
---
+++ src/APP/Database/tester.java
@@ -0,0 +1,11 @@
+public class CheckMySQLDriver {
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("MySQL JDBC Driver is successfully added to your classpath!");
+        } catch (ClassNotFoundException e) {
+            System.out.println("MySQL JDBC Driver is not found. Include it in your library path ");
+            e.printStackTrace();
+        }
+    }
+}
